fcad849022b4fbe2d6d61b21477f4e21b7f6718c
José Antônio <antoniojaraujo0@gmail.com>
2023-04-10 21:32:31 -0300

    connecting MongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
@@ -6,5 +10,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(`mongodb+srv://antoniojaraujo:${process.env.MONGODB_PASSWORD}@recipes.v9a2s95.mongodb.net/recipes?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.listen(3001, () => console.log('server started at port 3001'))
